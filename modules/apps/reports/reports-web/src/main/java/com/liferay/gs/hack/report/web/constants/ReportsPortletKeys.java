437b1316a04d7b349aca5ffcc6ff7015d3c1c9a1
Travis Cory <tcory@tcory-2.local>
2017-01-18 16:55:24 -0800

    Add more files for reports web

---
+++ modules/apps/reports/reports-web/src/main/java/com/liferay/gs/hack/report/web/constants/ReportsPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.gs.hack.report.web.constants;
+
+/**
+ * @author Travis Cory
+ */
+public class ReportsPortletKeys {
+
+	public static final String REPORTS = "com_liferay_gs_hack_report_web_portlet_ReportsPortlet";
+
+}
--- modules/apps/reports/reports-web/src/main/java/com/liferay/gs/hack/report/web/portlet/ReportsPortlet.java
+++ modules/apps/reports/reports-web/src/main/java/com/liferay/gs/hack/report/web/portlet/ReportsPortlet.java
@@ -1,36 +1,33 @@
 package com.liferay.gs.hack.report.web.portlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import com.liferay.gs.hack.report.web.constants.ReportsPortletKeys;
 
-import javax.portlet.GenericPortlet;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
 import javax.portlet.Portlet;
-import javax.portlet.PortletException;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Travis Cory
+ */
 
 @Component(
 	immediate = true,
 	property = {
+		"com.liferay.portlet.css-class-wrapper=reports-web",
 		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=reports-web Portlet",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Reports Portlet",
+		"javax.portlet.init-param.mvc-command-names-default-views=/reports/view",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.name=" + ReportsPortletKeys.REPORTS,
+		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=power-user,user"
 	},
 	service = Portlet.class
 )
-public class ReportsPortlet extends GenericPortlet {
-
-	@Override
-	protected void doView(
-			RenderRequest renderRequest, RenderResponse renderResponse)
-		throws IOException, PortletException {
-
-		PrintWriter printWriter = renderResponse.getWriter();
-
-		printWriter.print("reports-web Portlet - Hello World!");
-	}
-
+public class ReportsPortlet extends MVCPortlet {
 }
