a1f4c47de923e1336c5ce349ea8e2755d661d9de
Prathima Shreenath <prathima.shreenath@liferay.com>
2017-01-19 12:34:50 -0600

    INIT Tasks Web portlet

---
+++ modules/apps/tasks/tasks-web/src/main/java/com/liferay/gs/hack/tasks/web/portlet/constants/TaskWebConstants.java
@@ -0,0 +1,10 @@
+package com.liferay.gs.hack.tasks.web.portlet.constants;
+
+/**
+ * @author Prathima Shreenath
+ */
+public class TaskWebConstants {
+
+	public static final String TASK_PORTLET =
+		"com_liferay_gs_hack_tasks_web_portlet_TasksWebPortlet";
+}
