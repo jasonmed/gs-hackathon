f0253820ecef2ce2ae07b9d4d40e42bede701bf8
Prathima Shreenath <prathima.shreenath@liferay.com>
2017-01-19 13:51:15 -0600

    Add view

---
+++ modules/apps/tasks/tasks-web/src/main/java/com/liferay/gs/hack/tasks/web/portlet/ClientProjectProviderUtil.java
@@ -0,0 +1,37 @@
+package com.liferay.gs.hack.tasks.web.portlet;
+
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Prathima Shreenath
+ */
+public class ClientProjectProviderUtil {
+
+	public static List<Organization> getAllClients() {
+
+		List<Organization> clients = Collections.emptyList();
+
+		List<Organization> organizations =
+			OrganizationLocalServiceUtil.getOrganizations(-1, -1);
+
+		for (Organization organization : organizations) {
+			if (!organization.isParentable()) {
+
+				clients.add(organization);
+			}
+		}
+
+		return clients;
+	}
+
+	public static List<Organization> getAllProjects(
+		long companyId, long clientId) {
+
+		return OrganizationLocalServiceUtil.getSuborganizations(
+			companyId, clientId);
+	}
+}
