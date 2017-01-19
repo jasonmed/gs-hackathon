59fe9a7e04e56f44429b00ba1641ff318d3f3fae
Prathima Shreenath <prathima.shreenath@liferay.com>
2017-01-19 14:40:00 -0600

    Get Cient projects list

---
+++ modules/apps/timesheets/timesheets-web/src/main/java/com/liferay/gs/hack/timesheets/portlet/util/UserClientProjectProviderUtil.java
@@ -0,0 +1,50 @@
+package com.liferay.gs.hack.timesheets.portlet.util;
+
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Prathima Shreenath
+ */
+public class UserClientProjectProviderUtil {
+
+	public List<Organization> getAllUserClients(long userId) {
+
+		List<Organization> clients = Collections.emptyList();
+
+		List<Organization> organizations =
+			OrganizationLocalServiceUtil.getUserOrganizations(userId);
+
+		for (Organization organization : organizations) {
+			long parentId = organization.getParentOrganizationId();
+
+			if (parentId == 0) {
+				clients.add(organization);
+			}
+		}
+
+		return clients;
+	}
+
+	public List<Organization> getAllUserProjects(long userId) {
+
+		List<Organization> projects = Collections.emptyList();
+
+		List<Organization> organizations =
+			OrganizationLocalServiceUtil.getUserOrganizations(userId);
+
+		for (Organization organization : organizations) {
+			long parentId = organization.getParentOrganizationId();
+
+			if (parentId != 0) {
+				projects.add(organization);
+			}
+		}
+
+		return projects;
+	}
+}
